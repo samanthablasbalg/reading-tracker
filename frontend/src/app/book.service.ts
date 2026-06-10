08bbf8101b67f2d1b0bb9d7a891473a42218d06b
Samantha Blasbalg <blasbalgs@gmail.com>
2026-06-09 22:40:24 -0400

    feat: add the book service and list component

---
+++ frontend/src/app/book.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Author {
+  name: string;
+}
+
+export interface Book {
+  title: string;
+  authors: Author[];
+}
+
+@Injectable({ providedIn: 'root' })
+export class BookService {
+  private readonly http = inject(HttpClient);
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>('/api/books');
+  }
+}
