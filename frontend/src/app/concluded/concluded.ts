2541dbc8852d27c120be3ec4bb763591fbc5c247
Samantha Blasbalg <blasbalgs@gmail.com>
2026-06-21 19:49:31 -0400

    refactor: add headers to concluded page

--- frontend/src/app/concluded/concluded.ts
+++ frontend/src/app/concluded/concluded.ts
@@ -6,8 +6,10 @@ import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'app-concluded',
   imports: [ReadComponent, DNFComponent, MatDividerModule],
-  template: `<app-read />
+  template: `<h2 mat-dialog-title>Finished Books</h2>
+    <app-read />
     <mat-divider></mat-divider>
+    <h2 mat-dialog-title>DNFed Books</h2>
     <app-dnf />`,
 })
 export class ConcludedComponent {}
